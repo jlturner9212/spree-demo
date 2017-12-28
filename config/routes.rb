a284695ea31c4e01f37b641e9eb945ffee2b8625
Jeff Turner <jeff.turner00@gmail.com>
2017-12-28 17:15:04 -0500

    Add landing page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   #
   # We ask that you don't use the :as option here, as Spree relies on it being
   # the default of "spree".
-  mount Spree::Core::Engine, at: '/store'
+  mount Spree::Core::Engine, at: '/products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
